de3871cdaec6f4ea2c37289be5bcd5c5c8fdb5b1
1060352728 <43266944+1060352728@users.noreply.github.com>
2018-12-05 17:58:37 +0800

    代理解决跨域

--- config/index.js
+++ config/index.js
@@ -11,11 +11,11 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': {
-        target:'http://127.0.0.1:8099', // 你请求的第三方接口
-        changeOrigin:true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
-        pathRewrite:{  // 路径重写，
-          '^/api': '/api'  // 替换target中的请求地址，也就是说以后你在请求http://api.jisuapi.com/XXXXX这个地址的时候直接写成/api即可。
+      "/api":{
+        target:"http://127.0.0.1:8088",
+        changeOrigin:true,
+        pathRewrite:{
+          '^/api':''
         }
       }
     },
