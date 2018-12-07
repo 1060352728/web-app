4e372df6609fb9bee66c13c833b818bae7a63fae
1060352728 <43266944+1060352728@users.noreply.github.com>
2018-12-07 10:41:05 +0800

    主页添加子路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/member/Login'
 import Home from '@/components/home/Home'
-import Register from "@/components/member/Register";
+import Register from "@/components/member/Register"
+import Product from "@/components/product/Product"
+import Order from "@/components/order/Order"
 
 Vue.use(Router);
 
@@ -29,7 +31,19 @@ export default new Router({
       meta: {
         name: "跳转主页",
         requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
-      }
+      },
+      children: [
+        {
+          path: '/product',
+          name: 'Product',
+          component: Product
+        },
+        {
+          path: '/order',
+          name: 'Order',
+          component: Order
+        }
+      ]
     }
   ]
 })
