ebf164a06f4367ab6ae72ebf08a4c6316ab90be1
1060352728 <43266944+1060352728@users.noreply.github.com>
2018-12-10 18:10:54 +0800

    order获取订单列表

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
-    <h1>我是订单</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>地址</th>
+          <th>会员名称</th>
+          <th>手机号</th>
+          <th>总价</th>
+          <th>状态</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in orderList">
+          <td>{{item.buyerAddress}}</td>
+          <td>{{item.buyerName}}</td>
+          <td>{{item.buyerPhone}}</td>
+          <td>{{item.orderAmount}}</td>
+          <td>{{item.orderStatus|capitalize}}</td>
+          <td v-if="item.orderStatus === 0">
+            <a class="finishing" v-on:click="finishOrder(item.orderId)">完结订单</a>
+          </td>
+          <td v-else>
+            <a class="finished">完结订单</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -12,19 +38,37 @@
           orderList: ""
         }
       },
+      created () {
+        console.log("订单列表初始化。。。。。");
+        this.$axios({
+          method: 'get',
+          url: this.HOST+'/api-omc/order/list'
+        }).then(result=>{
+          this.orderList = result.data.data
+        })
+      },
       methods: {
-        getOrderList: function () {
+        finishOrder: function (orderId) {
           this.$axios({
             method: 'post',
-            url: this.HOST+'/api-omc/order/list'
+            url: this.HOST+'/api-omc/order/finish',
+            params: {"orderId":orderId}
           }).then(result=>{
+            if(result.data.data.code==200){
 
+            }
           })
+        }
+      },
+      filters: {
+        capitalize: function (value) {
+          return value == 0 ? "未完结" : "已完结";
         }
       }
     }
 </script>
 
 <style scoped>
-
+  .finishing{color: blue}
+  .finished{color: red}
 </style>
