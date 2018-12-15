724971a6cf3cebf974a34a0e51be67710f685c40
1060352728 <43266944+1060352728@users.noreply.github.com>
2018-12-15 17:34:27 +0800

    下单

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="main">
     <h1>我是产品</h1>
-    <table class="table" v-for="item in product.data">
+    <table class="table">
       <thead>
+        <tr>
+          <th><input type="checkbox" v-model="checked" v-on:click='checkedAll'></th>
+          <th>产品名称</th>
+          <th>产品描述</th>
+          <th>产品单价</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody v-for="item in product.data">
         <tr>
           <th>{{item.name}}</th>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><input type="checkbox"></td>
-          <td>产品名称</td>
-          <td>产品描述</td>
-          <td>产品单价</td>
-          <td>操作</td>
-        </tr>
         <tr v-for="food in item.foods">
-          <td><input type="checkbox" :value="food.id"></td>
+          <td><input type="checkbox" v-model='checkedList' :value="food.id" v-on:click="checkedOne(food.id)"></td>
           <td>{{food.name}}</td>
           <td>{{food.description}}</td>
           <td>{{food.price}}</td>
-          <td><button>+</button><input class="text" type="text"><button>-</button></td>
+          <td><button v-on:click="addno(food.id)">+</button><input readonly class="text" type="text" v-bind:value="productno[food.id]"><button v-on:click="subno(food.id)">-</button></td>
         </tr>
       </tbody>
     </table>
-    <div><button>下单</button></div>
+    <div><span class="address">请输入地址：</span><input type="text" v-model="address"><span class="address" v-if="addresserror">{{addresserror}}</span></div>
+    <div><button v-on:click="creatOrder">下单</button></div>
   </div>
 </template>
 
@@ -33,7 +34,16 @@
       name: "product",
       data () {
         return {
-          product: ""
+          product: "",
+          productno: {},
+          productIds: [],
+          checked: false,
+          checkedList: [],
+          address: "",
+          addresserror: "",
+          orderMsg: {
+            "name":"", "phone":"", "address":"", "openid":"","items":[]
+          }
         }
       },
       created () {
@@ -41,16 +51,68 @@
           method: 'get',
           url: this.HOST+'/api-psc/product/list',
         }).then(result=>{
-          console.log(result);
           if(result.data.code===0){
             this.product = result.data;
+            result.data.data.forEach(v=>{
+              v.foods.forEach(x=>{
+                this.$set(this.productno,x.id,1);
+                this.productIds.push(x.id);
+              });
+            });
           }
         })
+      },
+      methods: {
+        addno: function (id) {
+          this.productno[id] = this.productno[id] + 1;
+        },
+        subno: function (id) {
+          if(this.productno[id]>1){
+            this.productno[id] = this.productno[id] - 1;
+          }
+        },
+        checkedAll: function () {
+          this.checked = !this.checked;
+          if (this.checked) {
+            this.productIds.forEach(item=>{
+                this.checkedList.push(item)
+            });
+          } else {
+            this.checkedList = [];
+          }
+        },
+        checkedOne: function (checkedId) {
+          if(this.checkedList.indexOf(checkedId) > 0){
+            this.checkedList.splice(checkedId, 1)
+          }else{
+            this.checkedList.push(checkedId)
+          }
+        },
+        creatOrder: function () {
+          this.addresserror = "";
+          if(this.address==""){
+            this.addresserror = "请填写地址";
+            return false;
+          }
+          this.$axios({
+            method: 'post',
+            url: this.HOST+'/api-uac/user/findbyusername',
+            params:{
+              username: "seller"
+            }
+          }).then(result=>{
+            console.log(result.data)
+            this.orderMsg.name = result.data.username;
+            this.orderMsg.openid = result.data.openid
+          })
+        }
       }
     }
 </script>
 
 <style scoped>
   .main{text-align: center}
-  .text{width: 25px}
+  .text{width: 25px;}
+  .table{margin: auto}
+  .address{color: red}
 </style>
