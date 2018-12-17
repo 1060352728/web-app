28e311952e37bdb1b0c3f9ec86540b6324de842e
1060352728 <43266944+1060352728@users.noreply.github.com>
2018-12-17 11:24:13 +0800

    引入qs

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import App from './App'
 import store from './store/store'
 import router from './router'
 import axios from 'axios'
+import qs from 'qs';
 
+Vue.prototype.$qs = qs;
 Vue.prototype.$axios = axios;
 Vue.prototype.HOST = '/api';
 Vue.config.productionTip = false;
